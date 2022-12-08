099eba3256b2e36b1771bd79f43d923a68c11df0
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2022-12-08 12:54:34 -0300

    First commit

---
+++ src/main/java/com/arthurlunkes/primeiroAppUsandoSpring/PrimeiroAppUsandoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.arthurlunkes.primeiroAppUsandoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroAppUsandoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroAppUsandoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurlunkes/primeiroAppUsandoSpring/PrimeiroAppUsandoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurlunkes.primeiroAppUsandoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroAppUsandoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
